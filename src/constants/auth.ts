7e77cab4c7383cad21f464ff0b93516fd48d3804
pea3nut <pea3nut@qq.com>
2023-08-13 15:45:35 +0800

    fix(endpoint): add https endpoint

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,7 +1,7 @@
-export const officialAPIEndpoint = 'http://gpt-api.pea3nut.org/v1/chat/completions';
+export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
 const customAPIEndpoint =
-  import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'http://gpt-api.pea3nut.org/v1/chat/completions';
+  import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://gpt-api.pea3nut.org/v1/chat/completions';
 export const defaultAPIEndpoint =
-  import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
+  import.meta.env.VITE_DEFAULT_API_ENDPOINT || customAPIEndpoint;
 
-export const availableEndpoints = [officialAPIEndpoint, customAPIEndpoint];
+export const availableEndpoints = [customAPIEndpoint, officialAPIEndpoint];
